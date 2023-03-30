4661d403fad1999b695062d38f0854888650cfc6
Sandy Dwi Julian <128053268+Sandyyjulian@users.noreply.github.com>
2023-03-30 17:36:35 +0700

    Add files via upload

---
+++ Tugas1/fahrenheit.py
@@ -0,0 +1,15 @@
+#NAMA   : SANDY DWI JULIAN
+#NIM    : 210511033
+#KELAS  : K1
+
+def Fahrenheit(C):
+    F = (9/5*C)+32
+    return F
+
+print("Konversi Suhu dari Celcius")
+print("==========================")
+C = float(input("masukkan suhu dalam Celcius:"))
+F = Fahrenheit(C)
+print("celcius",str(C))
+print("-------------")
+print("Konversi ke Fahrenheit -", F)
